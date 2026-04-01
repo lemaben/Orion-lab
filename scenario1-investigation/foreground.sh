06fbca69d735704c6012b55ca93b31913bc46e63
lemaben <lemalemba.ben@gmail.com>
2026-04-01 08:56:35 +0200

    up

--- scenario1-investigation/foreground.sh
+++ scenario1-investigation/foreground.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "Préparation..."
-while [ ! -f /tmp/orion-bash-ready ]; do
+echo "Préparation de l'environnement d'investigation Orion..."
+while [ ! -f /tmp/orion-investigation-ready ]; do
   sleep 1
 done
 echo "Environnement prêt."
--- scenario2-recovery-hardening/foreground.sh
+++ scenario2-recovery-hardening/foreground.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "Préparation..."
-while [ ! -f /tmp/orion-bash-ready ]; do
+echo "Préparation de l'environnement de remédiation Orion..."
+while [ ! -f /tmp/orion-recovery-ready ]; do
   sleep 1
 done
 echo "Environnement prêt."
